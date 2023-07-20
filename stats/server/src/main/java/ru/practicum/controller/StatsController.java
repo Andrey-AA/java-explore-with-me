68a8c20f261154f4fb25593c546eb5d719ed3054
Andrey <abramov@okap.ru>
2023-07-20 20:20:49 +0300

    Первая часть диплома

---
+++ stats/server/src/main/java/ru/practicum/controller/StatsController.java
@@ -0,0 +1,42 @@
+package ru.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.dto.EndpointHitDto;
+import ru.practicum.dto.ViewStatsDto;
+import ru.practicum.service.StatsService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class StatsController {
+    private final StatsService statsService;
+
+    @PostMapping("/hit")
+    public ResponseEntity<EndpointHitDto> save(@RequestBody EndpointHitDto endpointHitDto) {
+        log.info("Data Entry logged {}", endpointHitDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(statsService.save(endpointHitDto));
+    }
+
+    @GetMapping("/stats")
+    public ResponseEntity<List<ViewStatsDto>> getStatistic(
+            @RequestParam(name = "start") String start,
+            @RequestParam(name = "end") String end,
+            @RequestParam(name = "uris", defaultValue = "") List<String> uris,
+            @RequestParam(name = "unique", defaultValue = "false") Boolean unique) {
+
+        log.info("Fetching stats: start date: {}, end date: {}, URIs: {}, unique: {}", start, end, uris, unique);
+        return ResponseEntity.ok(statsService.getStatistics(start, end, uris, unique));
+    }
+}
---
+++ stats/server/src/main/java/ru/practicum/service/StatsService.java
@@ -0,0 +1,14 @@
+package ru.practicum.service;
+
+import ru.practicum.dto.EndpointHitDto;
+import ru.practicum.dto.ViewStatsDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface StatsService {
+    EndpointHitDto save(EndpointHitDto endpointHitDto);
+
+    List<ViewStatsDto> getStatistics(String start, String end, List<String> uris, Boolean unique);
+
+}
